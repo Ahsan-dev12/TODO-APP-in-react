098fa2c5c4dedc01ca0dcaae3e383e79fef57a28
Ahsan-dev12 <wahsan769@gmail.com>
2025-04-26 11:24:50 +0500

    TODO operations

--- src/TodoReact.js
+++ src/TodoReact.js
@@ -1,41 +1,99 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TodoReact.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 
 function TodoReact() {
+
+    const [input, setInput] = useState("");
+    const [tasks, setTasks] = useState([]);
+    const [editIndex, setEditIndex] = useState(null);
+    
+    function handleInputChng(e){
+
+        setInput(e.target.value);
+    }
+    
+    // handle the add operation 
+    function handleAddTask(e){ 
+        e.preventDefault();
+        
+        if(editIndex !==null){
+            
+            const updatedTask = [...tasks];
+            updatedTask[editIndex] = input;
+            setTasks( updatedTask);
+            setEditIndex(null);
+            setInput(""); // input clear
+        }
+        else{
+
+            if(tasks.includes(input)){
+
+                alert("This task is already Existing")
+
+            }else{
+
+                setTasks([...tasks, input]); // input string hai, add ho jayega
+                setInput(""); // input clear
+
+            }
+
+        }
+        
+        console.log(tasks);
+    }
+
+    //handle the task delete operation
+    function handleDelete(recieveIndex){
+
+        const udatedTaskList = tasks.filter((task, index)=> index !== recieveIndex);
+        setTasks(udatedTaskList);
+
+    }
+
+    function handleEdit(reviceIndex){
+        
+        setInput(tasks[reviceIndex]);
+        setEditIndex(reviceIndex); //input field edit task ko set kr raha ha
+
+    }
+
+    // This the return  
   return (
+    
     <>
-    <div class="main">
-    <div class="txt">
+    <div className="main">
+    <div className="txt">
        <h3>To Do App</h3>
        <p>Now you can organize your daily task</p>
-       <input placeholder="Add your new task "/>
-       <button>Add Task</button>
+
+       <form onSubmit={handleAddTask}>
+          <input placeholder="Add your new task" value={input} onChange={handleInputChng}/>
+
+           <button type="submit">Add Task</button>
+       </form>
+
    </div>
 
-   <div class="taskTxt">
+   <div className="taskTxt">
     <h5>Your Task</h5>
     
-        <ul class="list-group">
-            <div>
-                <li class="list-group-item">
-                    An item
-
-                    {<span>
-                        <i class="bi bi-bag-check"></i>
-                        <i class="bi bi-trash"></i>
-                        <i class="bi bi-pen"></i>
-
-                    </span> }
+        <ul className="list-group">
+            {
+            tasks.map((task, index)=>(
+                <li key={index}>
+                    
+                    {task} --- {<span>
+                        <i className="bi bi-trash" onClick={()=>handleDelete(index)}></i>
+                        <i className="bi bi-pen " onClick={()=>handleEdit(index)}></i>
+                        </span> }
+                    
                 </li>
 
-                
-            </div>
-
-            <li class="list-group-item">A second item</li>
-            <li class="list-group-item">A third item</li>
-        
+            ))
+        }
+            
         </ul>
 
    </div>
